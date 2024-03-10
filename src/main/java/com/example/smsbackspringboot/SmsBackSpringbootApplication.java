fd1b0d748b48ee6732141750d85087b264e39371
鹅鹅 <1607435406@qq.com>
2024-03-10 21:08:42 +0800

    feat:Initial project

---
+++ src/main/java/com/example/smsbackspringboot/SmsBackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.smsbackspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsBackSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsBackSpringbootApplication.class, args);
+    }
+
+}
